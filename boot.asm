6c7dfe1a88d6005b23988e771783e537f493c4df
Obert Guo <obertguo@gmail.com>
2023-07-08 10:42:09 -0400

    read from disk

--- boot.asm
+++ boot.asm
@@ -1,27 +1,43 @@
 ;
 ; Simple boot sector program that includes a print function
 ;
+[bits 16]
+[org 0x7c00]    ; Set offset for where the boot program is loaded into memory
 
-[org 0x7c00] ; Set offset for where the boot program is loaded into memory
+mov [BOOT_DRIVE], dl    ; BIOS stores our boot drive in DL on startup
+                        ; so we'll need to remember this for later
+mov dh, 2   ; DH stores the # of sectors we want to read/load into memory
+            ; For now, let's read 2 sectors of the disk (each sector is 512B)
+mov dl, [BOOT_DRIVE] ; select which drive to load (0 indexed)
 
-; mov bx, my_string
-; call my_print
+mov bp, 0x8000 ; Set stack safely out of the way at 0x8000
+mov sp, bp
 
-; mov bx, my_string_2
-; call my_print
-mov bx, my_string
-jmp print_hex
+mov bx, 0x0000  ; Indirectly set address to read sectors to
+mov es, bx      ; by setting the starting segment
+mov bx, 0x9000  ; Now, set the segment offset. 
+                ; CPU translates this to 0x0000:0x9000 = 0x9000
+call disk_load
+call print_hex  ; Print hex value at mem addr of bx, where we read sectors to
+
 jmp $
 
-%include "print.asm"
+; IMPORTS
 %include "print_hex.asm"
+%include "print.asm"
+%include "disk_load.asm"
 
-; DATA
-my_string:
-    db "Hello World!", 0
-my_string_2:
-    db " World", 0
+; CONSTANTS
+BOOT_DRIVE:
+    db 0
+my_str:
+    db "test", 0
 
 ; Padding and magic number
 times 510 - ($ - $$) db 0
 dw 0xaa55
+
+; Some dummy values to fill up 2 sectors
+; Since each sector is usually 512 bytes
+times 256 dw 0xdada
+times 256 dw 0xface
---
+++ disk_load.asm
@@ -0,0 +1,45 @@
+disk_load:
+    pusha
+    push dx ; DH contains # of sectors to read
+    ; DL contains which drive to read from
+    ; push value of DX onto stack to use for comparison later (compare DH)
+    ; and since we are in 16 bit mode, we can only push DX and not DH
+
+    ; mov ah, 0x0e
+    ; mov al, dl
+    ; add al, '0'
+    ; int 0x10
+
+    mov ah, 0x02 ; BIOS interrupt for reading 
+
+    mov al, dh ; set number of sectors to read
+
+    mov ch, 0x00 ; select cylinder 0 
+    mov dh, 0x00 ; select track/head 0 (0 indexed)
+    mov cl, 0x02 ; start reading from 2nd sector (1 indexed)
+    
+    int 0x13 ; BIOS interrupt for disk read
+
+    jc disk_read_err ; jc = jump carry, jump if carry flag is set (read error)
+
+    pop dx ; restore dx to contain # sectors read
+    cmp al, dh ; AL is set to # of sectors read after calling disk read
+    ; BIOS interrupt. So, check if # sectors read matches what we expect
+    
+    jne disk_read_incomplete
+    
+    popa
+    ret
+disk_read_err:
+    mov bx, DISK_READ_ERR_MSG
+    call print_string
+    jmp $
+disk_read_incomplete:
+    mov bx, DISK_READ_INCOMPLETE_MSG
+    call print_string
+    jmp $
+
+DISK_READ_ERR_MSG:
+    db "Disk read failed!", 0
+DISK_READ_INCOMPLETE_MSG:
+    db "Disk read incomplete!", 0
