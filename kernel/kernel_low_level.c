53a5212439bfb23336e129709ca2949a745de897
obertguo <obertguo@gmail.com>
2023-07-23 10:46:29 -0400

    fix asm volatile

--- kernel/kernel_low_level.c
+++ kernel/kernel_low_level.c
@@ -1,6 +1,6 @@
 unsigned char port_byte_in(unsigned short port) {
     unsigned char result = 0;
-    __asm__(
+    __asm__ volatile(
         "in %%dx, %%al"     //  Assembler template, IN reads from I/O device
         :  "=a" (result)    //  Output operands (optional), variable result 
                             //      will store AL register
@@ -10,7 +10,7 @@ unsigned char port_byte_in(unsigned short port) {
 }
 
 void port_byte_out(unsigned short port, unsigned char data) {
-    asm volatile (
+    __asm__ volatile(
         "out %%al, %%dx"    // Assembler template, OUT writes to I/O device
         :
         : "a" (data), "d" (port)    // load EAX with data, load EDX with port
@@ -19,7 +19,7 @@ void port_byte_out(unsigned short port, unsigned char data) {
 
 unsigned char port_word_in(unsigned short port) {
     unsigned char result = 0;
-    asm volatile (
+    __asm__ volatile (
         "in %%dx, %%al"     //  Assembler template, IN reads from I/O device
         :  "=a" (result)    //  Output operands (optional), variable result 
                             //      will store AL register
@@ -29,7 +29,7 @@ unsigned char port_word_in(unsigned short port) {
 }
 
 void port_word_out(unsigned short port, unsigned short data) {
-    asm volatile (
+    __asm__ volatile(
         "out %%al, %%dx"    // Assembler template, OUT writes to I/O device
         :
         : "a" (data), "d" (port)    // load EAX with data, load EDX with port
