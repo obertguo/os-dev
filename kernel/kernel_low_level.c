de7de6f109bf831ce611142faa75cce8b77832b4
obertguo <obertguo@gmail.com>
2023-07-23 10:43:33 -0400

    use volatile for inline asm

--- kernel/kernel_low_level.c
+++ kernel/kernel_low_level.c
@@ -10,7 +10,7 @@ unsigned char port_byte_in(unsigned short port) {
 }
 
 void port_byte_out(unsigned short port, unsigned char data) {
-    __asm__(
+    asm volatile (
         "out %%al, %%dx"    // Assembler template, OUT writes to I/O device
         :
         : "a" (data), "d" (port)    // load EAX with data, load EDX with port
@@ -19,7 +19,7 @@ void port_byte_out(unsigned short port, unsigned char data) {
 
 unsigned char port_word_in(unsigned short port) {
     unsigned char result = 0;
-    __asm__(
+    asm volatile (
         "in %%dx, %%al"     //  Assembler template, IN reads from I/O device
         :  "=a" (result)    //  Output operands (optional), variable result 
                             //      will store AL register
@@ -29,7 +29,7 @@ unsigned char port_word_in(unsigned short port) {
 }
 
 void port_word_out(unsigned short port, unsigned short data) {
-    __asm__(
+    asm volatile (
         "out %%al, %%dx"    // Assembler template, OUT writes to I/O device
         :
         : "a" (data), "d" (port)    // load EAX with data, load EDX with port
